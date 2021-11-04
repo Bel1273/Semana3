3805acf3767781f1909d5218c783658abe942031
Bel1273 <71009373+Bel1273@users.noreply.github.com>
2021-11-04 16:32:09 +0100

    Add files via upload

---
+++ Mascota_Recycler_ActionView_S3/app/src/main/java/almachsystems/com/mascota_recycler_actionview_s3/MainActivity.java
@@ -0,0 +1,14 @@
+package almachsystems.com.mascota_recycler_actionview_s3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
